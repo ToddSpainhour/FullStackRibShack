0d1aeeb7ca4fde1b17df8829fa981dd4eb7f451c
Todd Spainhour <toddspainhour@gmail.com>
2021-01-26 20:13:06 -0600

    added services.AddTransient for ScheduleRepository

--- FullStackRibShack/Startup.cs
+++ FullStackRibShack/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using FullStackRibShack.Data;
 
 namespace FullStackRibShack
 {
@@ -26,6 +27,8 @@ namespace FullStackRibShack
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddTransient<ScheduleRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
