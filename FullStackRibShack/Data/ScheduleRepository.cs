2ce8eeed18288b97e8b1a23ebd334d74111adc24
Todd Spainhour <toddspainhour@gmail.com>
2021-01-29 23:11:15 -0600

    SQL query to return next five events WIP

--- FullStackRibShack/Data/ScheduleRepository.cs
+++ FullStackRibShack/Data/ScheduleRepository.cs
@@ -12,13 +12,22 @@ namespace FullStackRibShack.Data
     {
         const string _connectionString = "Server = localhost; Database = FullStackRibShack; Trusted_Connection = True;";
 
+
+        // returns all events from Schedule table
         public List<Schedule> GetAll()
         {
             using var db = new SqlConnection(_connectionString);
 
+            var test = DateTime.Today;
+
+            Console.WriteLine(test);
+
             try
             {
-                var fullSchedule = db.Query<Schedule>("SELECT * FROM Schedule");
+                var fullSchedule = db.Query<Schedule>
+                    ("SELECT * " +
+                    "FROM Schedule " +
+                    "ORDER BY Date ASC");
 
                 return fullSchedule.ToList();
             }
@@ -27,6 +36,41 @@ namespace FullStackRibShack.Data
                 Console.WriteLine(e);
                 throw;
             }
+        }
+
+
+        // returns next 5 scheduled events
+        public List<Schedule> GetNextFiveEvents()
+        {
+            using var db = new SqlConnection(_connectionString);
+
+            
+            try
+            {
+
+                //var nextFiveEvents = db.Query<Schedule>
+                //    ("DECLARE @TodaysDate DateTime " +
+                //    "SET @TodaysDate = GetDate() " +
+                //    "SELECT *" +
+                //    "FROM Schedule " +
+                //    "WHERE Date >= @TodaysDate");
+
+                var nextFiveEvents = db.Query<Schedule>
+                   ("DECLARE @TodaysDate DateTime " +
+                   "SET @TodaysDate = Test " +
+                   "SELECT *" +
+                   "FROM Schedule " +
+                   "WHERE Date >= @TodaysDate");
+
+                return nextFiveEvents.ToList();
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw; 
+            }
+
         }
     }
 }
