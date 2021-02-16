e053c9471b7f9b1a687f6e4866d34fdf6609eca1
Todd Spainhour <toddspainhour@gmail.com>
2021-02-15 21:41:50 -0600

    initial data call puts schedule data in state

--- fullstackribshack.ui/src/components/Schedule/Schedule.js
+++ fullstackribshack.ui/src/components/Schedule/Schedule.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import baseUrl from '../../helpers/data/constants';
 
 import './Schedule.scss';
 
 function Schedule() {
-  return (
-        <div className="Schedule">
+  const [events, setAllScheduledEvents] = useState('');
+
+  const getAllScheduledEvents = () => {
+    axios.get(`${baseUrl}/schedule`)
+
+      .then((response) => {
+        const AllEvents = response.data;
+        setAllScheduledEvents(AllEvents);
+      })
+      .catch((error) => console.error(`Error: ${error}`));
+  };
+
+  useEffect(() => {
+    getAllScheduledEvents();
+  }, []);
+
+  if (events.length > 0) {
+    return (
+      <div className="Schedule">
           <h6>Schedule component</h6>
-        </div>
-  );
+          <h6>{events[0].timeOpen}</h6>
+      </div>
+    );
+  }
+  return (<h3>Loading...</h3>);
 }
 
 export default Schedule;
