bbd334bcf50fe488a2fe96f5a6584e5d06482e0e
Todd Spainhour <toddspainhour@gmail.com>
2021-03-02 21:41:55 -0600

    added bootstrap navbar component

---
+++ fullstackribshack.ui/src/components/shared/MyNavbar/MyNavbar.js
@@ -0,0 +1,31 @@
+import React from 'react';
+// simport Navbar from 'react-bootstrap/Navbar'
+
+import './MyNavbar.scss';
+
+function MyNavbar() {
+  return (
+
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <a className="navbar-brand" href="app-component">Full Stack Rib Shack</a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+  <div className="collapse navbar-collapse" id="navbarNav">
+    <ul className="navbar-nav">
+      <li className="nav-item active">
+        <a className="nav-link">Menu<span className="sr-only"></span></a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#schedule-component">Schedule</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link">About Us</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+  );
+}
+
+export default MyNavbar;
