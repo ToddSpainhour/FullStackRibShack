9c79d7931fa5c043e008ebca0ea34318abf675e3
Todd Spainhour <toddspainhour@gmail.com>
2021-03-06 12:05:20 -0600

    navbar expanding and collapsing on mobile sized screens

--- fullstackribshack.ui/src/components/shared/MyNavbar/MyNavbar.js
+++ fullstackribshack.ui/src/components/shared/MyNavbar/MyNavbar.js
@@ -1,17 +1,19 @@
-import React from 'react';
-// simport Navbar from 'react-bootstrap/Navbar'
-
 import './MyNavbar.scss';
+import { useState } from 'react';
 
 function MyNavbar() {
+  const [isNavbarCollapsed, setNavbarCollapsed] = useState(false);
+
+  const handleNavbarCollapse = () => setNavbarCollapsed(!isNavbarCollapsed);
+
   return (
 
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
   <a className="navbar-brand" href="app-component">Full Stack Rib Shack</a>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+  <button className="navbar-toggler" type="button" onClick={handleNavbarCollapse} data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
-  <div className="collapse navbar-collapse" id="navbarNav">
+  <div className={`${isNavbarCollapsed ? 'collapse' : ''} navbar-collapse`} id="navbarNav">
     <ul className="navbar-nav">
       <li className="nav-item active">
         <a className="nav-link">Menu<span className="sr-only"></span></a>
