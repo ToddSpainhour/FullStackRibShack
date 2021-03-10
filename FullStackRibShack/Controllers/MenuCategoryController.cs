1cc3b700fc22a540aceaeb1f86049214ab89076e
Todd Spainhour <toddspainhour@gmail.com>
2021-03-09 19:46:21 -0600

    added MenuCategory endpoint

---
+++ FullStackRibShack/Controllers/MenuCategoryController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using FullStackRibShack.Data;
+
+namespace FullStackRibShack.Controllers
+{
+    [Route("api/menuCategories")]
+    [ApiController]
+    public class MenuCategoryController : Controller
+    {
+        MenuCategoryRepository _repo;
+
+        public MenuCategoryController(MenuCategoryRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpGet]
+        public IActionResult GetMenuCategories()
+        {
+            var allMenuCategories = _repo.GetAll();
+
+            return Ok(allMenuCategories);
+        }
+
+    }
+}
---
+++ FullStackRibShack/Models/MenuCategoryModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FullStackRibShack.Models
+{
+    public class MenuCategoryModel
+    {
+        public int Id { get; set; }
+        public string MenuCategory { get; set; }
+    }
+}
