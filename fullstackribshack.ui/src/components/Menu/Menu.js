e2701035b04bb4a4c79bf80ecceecd30e76f2234
Todd Spainhour <toddspainhour@gmail.com>
2021-04-17 11:24:01 -0500

    created Menu component and scss file

---
+++ fullstackribshack.ui/src/components/Menu/Menu.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+
+import './Menu.scss';
+
+function Menu() {
+  return (
+    <div className='Menu'>
+      <h2>Menu Component</h2>
+    </div>
+  );
+}
+
+export default Menu;
